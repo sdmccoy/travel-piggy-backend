1063b48a6c09b51725eccba30f8571595ac9177a
Scott <sdmccoy@gmail.com>
2017-11-13 19:15:58 -0800

    post route working

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const pg = require('pg');
-
+// const {Pool} = require('pg');
+// console.log('pool = ', Pool);
 //configure PostgreSQL connection here
 const conString = 'postgres://postgres:1234@localhost:5432/travelpiggy';
 //create new pg client with constring connection.
 const client = new pg.Client(conString);
-//using the client object and the connect method to connect.
 client.connect();
+//using the client object and the connect method to connect.
+module.exports = client;
--- src/db/user-table.js
+++ src/db/user-table.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const client = require('./connect-db.js');
+// console.log('newdb = ', newDB);
+// const client = newDB.connect();
+console.log('client = ', client);
+const {Router} = require('express');
 //load the db on server start
 function loadDB(){
 //create a table if table doesn't exist
@@ -14,3 +18,27 @@ function loadDB(){
     .catch( error => console.log('error = ', error));
 }
 loadDB();
+
+const userRouter = module.exports = new Router();
+
+//create new user
+userRouter.post('/user', (req, res, next) => {
+  console.log('req.body = ', req.body);
+
+  client.query(`
+    INSERT INTO users(user_id, token, "email")
+    VALUES ($1, $2, $3);
+    `,
+    [
+      req.body.user_id,
+      req.body.token,
+      req.body.email
+    ]
+  )
+    .then(result => {
+      res.send('new user inserted');
+      console.log('res = ', res.body);
+    })
+    .catch(next);
+}
+);
